50a5b1bd309a258497a1ac2ca1ebe1b5b6362b76
Ferdy Fermadi <ferdy.fermadi99@gmail.com>
2024-07-23 11:44:58 +0700

    [ADD] Create Tabs Navigator for home, transaction and whislist

---
+++ app/(tabs)/transaction.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Transaction = () => {
+    return (
+        <View>
+            <Text>Transaction</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Transaction;
