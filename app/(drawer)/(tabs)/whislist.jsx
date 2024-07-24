625b202444f62eace1cc83d27816d8c00a49e83d
Ferdy Fermadi <ferdy.fermadi99@gmail.com>
2024-07-24 16:49:51 +0700

    [ADD] create whislist

--- app/(drawer)/(tabs)/whislist.jsx
+++ app/(drawer)/(tabs)/whislist.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, ScrollView, StyleSheet } from "react-native";
+import CardProduct from "../../../components/CardProduct";
 
-const Whislist = () => {
-    return (
-        <View>
-            <Text>Whislist</Text>
+const Wishlist = () => {
+  return (
+    <ScrollView style={styles.container}>
+        <View style={styles.cardProductList}>
+            <CardProduct category="Category" name="Product 1" seller="Seller 1" price="$100" />
+            <CardProduct category="Category" name="Product 2" seller="Seller 2" price="$200" />
         </View>
-    );
-}
+    </ScrollView>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+    padding: 10,
+  },
+  cardProductList: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
 
-export default Whislist;
+export default Wishlist;
