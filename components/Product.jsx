226309a2ce3af7227651fe7635dffce97548fb56
Ayu <alestari.msh@gmail.com>
2024-07-25 19:32:55 +0700

    [ADD] Create Transaction Screen

---
+++ components/Product.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const Product = ({ name, quantity, price }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: "https://via.placeholder.com/50" }} style={styles.image} />
+      <View style={styles.details}>
+        <Text>{name}</Text>
+        <Text>{quantity}</Text>
+        <Text>{price}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 10,
+    margin: 5,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  details: {
+    marginLeft: 10,
+  },
+});
+
+export default Product;
