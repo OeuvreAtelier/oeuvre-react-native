2fbb4d0f7c4b6b3487c8a6561cd3daaf59224b33
Ayu <alestari.msh@gmail.com>
2024-08-01 16:17:24 +0700

    [ADD] Address Form

--- app/(drawer)/(screen)/address.jsx
+++ app/(drawer)/(screen)/address.jsx
@@ -1,14 +1,66 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// AddressScreen.js
+import React, { useEffect } from 'react';
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { useRouter } from 'expo-router';
+import { fetchUser } from '../../../redux/features/userSlice';
+import { fetchAddressByUserId } from '../../../redux/features/addressSlice';
 
-const Address = () => {
-    return (
-        <View>
-            <Text>Address</Text>
-        </View>
-    );
+export default function AddressScreen() {
+  const dispatch = useDispatch();
+  const router = useRouter(); 
+  const user = useSelector((state) => state.user.data);
+  const addresses = useSelector((state) => state.address);
+
+  useEffect(() => {
+    dispatch(fetchUser())
+    if(user){
+
+        dispatch(fetchAddressByUserId(user.id
+        ))
+    }
+  }, [dispatch])
+
+
+  const handleEdit = (address) => {
+    router.push({
+      pathname: 'addressForm',
+      params: { state: JSON.stringify({ address }) },
+    });
+  };
+
+  const handleAdd = () => {
+    router.push({
+      pathname: 'addressForm',
+      params: { state: JSON.stringify(null) },
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={addresses}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.addressItem}>
+            <Text>{item.detail}</Text>
+            <Button title="Edit" onPress={() => handleEdit(item)} />
+          </View>
+        )}
+      />
+      <Button title="Add Address" onPress={handleAdd} />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
-
-export default Address;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  addressItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+});
